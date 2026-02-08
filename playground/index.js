582349bfe8ed22fae63dad19fbbd97c7a887d60a
Costantino Dionis Gwaka <gwaka94@gmail.com>
2026-02-08 16:14:55 +0300

    simple app with api

--- playground/index.js
+++ playground/index.js
@@ -1,10 +1,33 @@
 
-//es module
-import { get_encoding } from "tiktoken";
+// //es module
+// import { get_encoding } from "tiktoken";
 
 
-//token id --> token
-//904 --> hello
-const encoding =  get_encoding('cl100k_base');
-const token = encoding.encode("Hello world! this is first test");
-console.log(token);
+// //token id --> token
+// //904 --> hello
+// const encoding =  get_encoding('cl100k_base');
+// const token = encoding.encode("Hello world! this is first test");
+// console.log(token);
+
+
+import OpenAI from "openai";
+
+const OPEN_API_KEY = 'sk-proj-yBhVj_-9A0EopT1A06bxRFdqBSUx-YbRVGsJUE8-KkvWx-9v7NqBDlovQgbFs9LEaO2Xu3rYrz1gRdnW62MeDpwYvl7Lqi0udMiUJJYovnBGAA';
+
+const client = new OpenAI({
+    apiKey: OPEN_API_KEY
+});
+
+const stream = await client.responses.create({
+    model: 'gpt-4.1',
+    input:'Write a story about a robot',
+    temperature:0.7,
+    max_output_tokens: 250,
+    stream: true
+})
+
+// console.log(answer);
+for await(const event of stream){
+    if(event.delta)
+        process.stdout.write(event.delta);
+}
