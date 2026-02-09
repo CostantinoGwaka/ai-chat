3b61663f88a835c4893647422c6a3e7dbcf17f11
Costantino Dionis Gwaka <gwaka94@gmail.com>
2026-02-09 12:14:15 +0300

    refactor services

--- myapp/packages/server/index.ts
+++ myapp/packages/server/index.ts
@@ -2,14 +2,13 @@ import express from "express";
 import type { Request, Response } from "express";
 import dotenv from "dotenv";
 import OpenAI from "openai";
+import z from "zod";
+import { conversationRepository } from "./repositories/conversation.repository";
+import { chatService } from "./services/chat.services";
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
-
-const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
@@ -19,17 +18,30 @@ app.get("/api/hello", (req: Request, res: Response) => {
   res.json({ message: "Hello World" });
 });
 
+const chatSchema = z.object({
+  prompt: z
+    .string()
+    .trim()
+    .min(1, "Prompt is required")
+    .max(1000, "Prompt is too long(max 1000 character)"),
+  conversationId: z.string().uuid(),
+});
+
 app.post("/api/chat", async (req: Request, res: Response) => {
-  const { prompt } = req.body;
+  const parseResult = chatSchema.safeParse(req.body);
 
-  const response = await client.responses.create({
-    model: "gpt-4o-mini",
-    input: prompt,
-    temperature: 0.2,
-    max_output_tokens: 100,
-  });
+  if (!parseResult.success) {
+    res.status(400).json(parseResult.error.format());
+    return;
+  }
 
-  res.json({ message: response.output_text });
+  try {
+    const { prompt, conversationId } = req.body;
+    const response = await chatService.sendMessage(prompt, conversationId);
+    res.json({ message: response.message });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to generate response" });
+  }
 });
 
 app.listen(port, () => {
---
+++ myapp/packages/server/repositories/conversation.repository.ts
@@ -0,0 +1,22 @@
+// implementation detail
+// let lastResponseId: string | null = null;
+const conversations = new Map<string, string>();
+
+export const conversationRepository = {
+  getLastResponseId(conversationId: string) {
+    return conversations.get(conversationId);
+  },
+
+  setLastResponseId(conversationId: string, responseId: string) {
+    conversations.set(conversationId, responseId);
+  },
+};
+
+// //export  public interface
+// export function getLastResponseId(conversationId: string) {
+//   return conversations.get(conversationId);
+// }
+
+// export function setLastResponseId(conversationId: string, responseId: string) {
+//   conversations.set(conversationId, responseId);
+// }
